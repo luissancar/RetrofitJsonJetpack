3d4b764c08f5ac240bfddaf049918e24c5585a8a
luis jose sanchez carrasco <luissancar@yahoo.es>
2021-11-19 15:11:37 +0100

    final

--- app/src/main/java/com/example/retrofitjsonjetpack/MainActivity.kt
+++ app/src/main/java/com/example/retrofitjsonjetpack/MainActivity.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -19,8 +26,12 @@ import retrofit2.Response
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-
-
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 
 
 //https://www.youtube.com/watch?v=Ry7VvxDgrbk
@@ -47,7 +58,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Llamada() {
     val contest= LocalContext.current
-   // lateinit var  userI:UserInfo
+
     var users by rememberSaveable { mutableStateOf(UserInfo()) }
 
     val user=UserInstance.userInterface.userInformation()
@@ -74,6 +85,23 @@ fun Llamada() {
     })
 
 
-    Text(text = users.toString())
+    LazyColumn()
+    
+    {
+        items(users){
+            usu -> 
+            Image(painter =  rememberImagePainter(usu.avatar_url),
+                contentDescription = "Imagen",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(100.dp)),
+                contentScale = ContentScale.FillWidth
+                 )
+
+        }
+    }
+   // Text(text = users.toString())
 
 }
